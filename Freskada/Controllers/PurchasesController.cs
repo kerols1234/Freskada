7df0c21b972f30738a717ad7ea0e7685b7c4731c
kerols <62767480+kerols1234@users.noreply.github.com>
2023-03-27 19:20:12 +0200

    purchase controller

---
+++ Freskada/Controllers/PurchasesController.cs
@@ -0,0 +1,110 @@
+﻿using Freskada.Data;
+using Freskada.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freskada.Controllers
+{
+    [Authorize]
+    [EnableCors]
+    public class PurchasesController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public PurchasesController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            var selectListItems = _context.Materials.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString(),
+            }).ToList();
+
+            var purchase = new Purchase();
+            if (id != null)
+            {
+                purchase = _context.Purchases.FirstOrDefault(obj => obj.Id == id);
+            }
+            return View(purchase);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpsertAsync(Purchase model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Id != 0)
+                {
+                    if (model.Date == null)
+                    {
+                        model.Date = _context.Expenses.AsNoTracking().FirstOrDefault(obj => obj.Id == model.Id).Date;
+                    }
+                    _context.Purchases.Update(model);
+                    TempData[SD.Success] = "Purchase updated successfully";
+                }
+                else
+                {
+                    model.User = (ApplicationUser)await _userManager.GetUserAsync(User);
+                    model.Date = model.Date ?? DateTime.Now;
+                    _context.Purchases.Add(model);
+                    TempData[SD.Success] = "Purchase created successfully";
+                }
+                _context.SaveChanges();
+            }
+
+            return Redirect(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPurchases()
+        {
+            return Json(new
+            {
+                data = await _context.Purchases.Include(obj => obj.User).Select(obj => new
+                {
+                    purchase = new
+                    {
+                        obj.Id,
+                        obj.User.EmployeeName,
+                        PurchaseMaterials = obj.PurchaseMaterials.Count,
+                        Date = obj.Date.GetValueOrDefault().ToString("dd/MM/yyyy hh:mm tt"),
+                        Price = obj.PurchaseMaterials.Sum(s => s.Price),
+                    }
+                }).ToListAsync()
+            });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePurchase(int id)
+        {
+            var purchase = await _context.Purchases.FirstOrDefaultAsync(obj => obj.Id == id);
+            if (purchase == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _context.Purchases.Remove(purchase);
+            _context.SaveChanges();
+            return Json(new { success = true, message = "Purchase deleted successfully" });
+        }
+    }
+}
---
+++ Freskada/wwwroot/js/purchase.js
@@ -0,0 +1,108 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#t_purchase').DataTable({
+        "ajax": {
+            "url": "/Purchases/GetAllPurchases/",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "stateSave": true,
+        "stateSaveCallback": function (settings, data) {
+            localStorage.setItem('DataTables_' + settings.sInstance, JSON.stringify(data))
+        },
+        "stateLoadCallback": function (settings) {
+            return JSON.parse(localStorage.getItem('DataTables_' + settings.sInstance))
+        },
+        "lengthChange": false,
+        "autoWidth": true,
+        "columns": [
+            { "data": "purchase.employeeName" },
+            { "data": "purchase.date" },
+            { "data": "purchase.price" },
+            { "data": "purchase.purchaseMaterials" },
+            {
+                "data": "purchase",
+                "render": function (data) {
+                    let obj = JSON.stringify(data);
+                    obj = obj.replace("'", "\\'");
+                    obj = obj.replaceAll("\\r\\n", "<br>");
+
+                    let dev = document.createElement("div");
+                    let infoBtn = document.createElement("button");
+                    let editBtn = document.createElement("button");
+                    let editIcon = document.createElement("i");
+                    let infoIcon = document.createElement("i");
+                    let deleteIcon = document.createElement("i");
+                    let a = document.createElement("a");
+
+                    dev.classList.add("text-center");
+
+                    editBtn.classList.add("btn", "btn-success", "text-white", "btn-circle", "mx-1");
+                    infoBtn.classList.add("btn", "btn-info", "text-white", "btn-circle", "mx-1");
+
+                    a.style.cursor = "pointer";
+                    a.classList.add("btn", "btn-danger", "text-white", "btn-circle", "mx-1");
+
+                    editIcon.classList.add("fa", "fa-pencil-alt");
+                    infoIcon.classList.add("fa", "fa-book");
+                    deleteIcon.classList.add("fa-solid", "fa-x");
+
+                    editBtn.setAttribute("data-bs-target", "#purchaseForm");
+                    editBtn.setAttribute("data-bs-toggle", "modal");
+                    editBtn.setAttribute("onclick", `modelEditclicked('${obj}')`);
+
+                    infoBtn.setAttribute("data-bs-target", "#purchasesDetails");
+                    infoBtn.setAttribute("data-bs-toggle", "modal");
+                    infoBtn.setAttribute("onclick", `modelInfoclicked('${obj}')`);
+
+                    a.setAttribute("onclick", `Delete('/Purchases/DeletePurchase?id=${data.id}')`);
+
+                    editBtn.appendChild(editIcon);
+                    infoBtn.appendChild(infoIcon);
+                    a.appendChild(deleteIcon);
+                    dev.appendChild(editBtn);
+                    dev.appendChild(infoBtn);
+                    dev.appendChild(a);
+
+                    return dev.outerHTML;
+                }
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        cancelButtonText: "Cancel"
+    }).then((result) => {
+        if (result.value) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
